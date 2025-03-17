cdf695316305e8ffb6b58925e3218b85cea977af
Jonatas Venancio <jvenancio167@hotmail.com>
2025-03-17 20:46:15 -0300

    create frontend project with vuejs and tailwind

---
+++ front-end/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="container mx-auto">
+    <h1 class="text-3xl font-bold underline bg-red-500 text-white p-4">
+      Hello world!
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
