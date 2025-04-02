e6631acccc20f67d0eeac11c541290ea23e8ebed
Jonatas Venancio <jvenancio167@hotmail.com>
2025-04-01 23:19:58 -0300

    create button component

---
+++ front-end/src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button :type="type" :disabled="disabled" :class="className">
+    <slot />
+  </button>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'Button',
+  props: {
+    type: {
+      type: String,
+      default: 'button',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+});
+</script>
+
+<style scoped>
+  button {
+    cursor: pointer;
+  }
+</style>
