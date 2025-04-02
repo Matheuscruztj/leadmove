a2ab0319e7de0ca61ae2dc4080b9376bfc62e63b
Jonatas Venancio <jvenancio167@hotmail.com>
2025-04-01 23:20:45 -0300

    create sidebar component

--- front-end/src/components/SidebarComponent.vue
+++ front-end/src/components/SidebarComponent.vue
@@ -1,6 +1,6 @@
 <template>
-  <div :class="['flex flex-col h-screen bg-purple-700 text-white transition-all duration-300', isCollapsed ? 'w-20' : 'w-52 md:w-48']">
-    <div class="flex items-center justify-between h-16 bg-purple-800 px-4">
+  <div :class="['flex flex-col h-screen bg-purple text-white transition-all duration-300', isCollapsed ? 'w-20' : 'w-52 md:w-48']">
+    <div class="flex items-center justify-between h-16 bg-purple px-4">
       <h1 class="text-2xl font-bold transition-opacity duration-300" v-if="!isCollapsed">Sidebar</h1>
       <button @click="toggleCollapse" class="text-white focus:outline-none">
         <transition name="fade" mode="out-in">
@@ -16,7 +16,7 @@
     <nav class="flex-1 px-4 py-2">
       <ul>
         <li class="py-2" v-for="item in menuItems" :key="item.name">
-          <a :href=item.href class="px-4 py-2 rounded flex items-center hover:bg-purple-600 transition-all duration-300 transform hover:-translate-y-1" :class="{'opacity-0': isCollapsed, 'opacity-100': !isCollapsed}">
+          <a :href=item.href class="px-4 py-2 rounded flex items-center hover:bg-pink-700 transition-all duration-300 transform hover:-translate-y-1" :class="{'opacity-0': isCollapsed, 'opacity-100': !isCollapsed}">
             <component :is="item.icon" class="h-6 w-6 mr-2" />
             {{ item.name }}
           </a>
