35d1c9548325b7115aa8632ae97ff00456d1ef62
Jonatas Venancio <jvenancio167@hotmail.com>
2025-04-08 23:46:26 -0300

    create table component

---
+++ front-end/src/components/Table.vue
@@ -0,0 +1,21 @@
+<template>
+  <table class="table-auto">
+    <thead>
+      <tr class="bg-gray-200">
+        <slot name="header"></slot>
+      </tr>
+    </thead>
+    <tbody>
+      <slot name="body"></slot>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import { ref } from 'vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: "Table"
+})
+</script>
