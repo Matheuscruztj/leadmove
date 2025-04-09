08b8eda783fa931d8ce106b8029dc4cdf5e763a9
Jonatas Venancio <jvenancio167@hotmail.com>
2025-04-08 23:49:16 -0300

    create route to component

--- front-end/src/router/router.ts
+++ front-end/src/router/router.ts
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'; 
 import Home from '../views/Home.vue';
 import User from '../views/User.vue';
+import Scheduler from '../views/scheduler/schedule.vue';
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: Home
+  },
+  {
+    path: '/schedule',
+    name: 'schedule',
+    component: Scheduler
   },
   {
     path: '/user',
