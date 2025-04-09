671710d30346cf27edca1e87be0acd85cbc2f20d
Jonatas Venancio <jvenancio167@hotmail.com>
2025-04-08 23:47:55 -0300

    change color to toggle component when user check as true

--- front-end/src/components/Form/Toggle.vue
+++ front-end/src/components/Form/Toggle.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="modelValue ? 'bg-purple-500' : 'bg-gray-300'" @click="toggle" class="w-11 transition-all duration-500 h-6 flex items-center rounded-full p-0.5">
+  <div :class="modelValue ? 'bg-orange-500' : 'bg-gray-300'" @click="toggle" class="w-11 transition-all duration-500 h-6 flex items-center rounded-full p-0.5">
     <div :class="{'translate-x-5': modelValue}" class="w-[1.2rem] h-[1.2rem] transition-all duration-500 transform bg-white rounded-full shadow-md"></div>
   </div>
 </template>
