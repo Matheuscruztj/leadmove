2c3604dc142aedaa1d12803156b7c1ffe8e9baae
Jonatas Venancio <jvenancio167@hotmail.com>
2025-04-10 23:00:24 -0300

    add i18n to the project

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router/router'
+import i18n from './i18n/pt'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(i18n).mount('#app')
