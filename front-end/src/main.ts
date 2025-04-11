3110c12c96c507f3c8651c3e2995e2031d3a0245
Jonatas Venancio <jvenancio167@hotmail.com>
2025-04-11 00:43:01 -0300

    separated the app.use to setupPlugins

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import router from './router/router'
-import i18n from './i18n/pt'
+import { setupPlugins } from './plugins'
 
-createApp(App).use(router).use(i18n).mount('#app')
+const app = createApp(App)
+setupPlugins(app)
+
+app.mount('#app')
