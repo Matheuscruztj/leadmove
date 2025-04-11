60d28458d1b67dfbd4a46d6c8db816969e1c0e05
Jonatas Venancio <jvenancio167@hotmail.com>
2025-04-10 23:25:08 -0300

    remove svg in the modal

--- front-end/src/components/Modal/Modal.vue
+++ front-end/src/components/Modal/Modal.vue
@@ -5,7 +5,7 @@
       class="fixed z-10 inset-0 flex items-center justify-center bg-black bg-opacity-50"
       @click="$emit('close')"
     >
-      <div class="relative w-full max-w-lg max-h-[90vh] overflow-y-auto p-4" @click.stop>
+      <div class="relative w-full max-w-lg max-h-[90vh] overflow-y-auto p-4">
         <div class="relative bg-white rounded-lg shadow-xl ">
           <!-- Modal Header -->
           <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
@@ -17,21 +17,7 @@
               class="cursor-pointer text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
               @click="$emit('close')"
             >
-              <svg
-                class="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                />
-              </svg>
+              x
               <span class="sr-only">Fechar modal</span>
             </button>
           </div>
